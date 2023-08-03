a16c9e343cff82d26364fecdd85f426a1d8d5e60
Anas <anasna6005@gmail.com>
2023-08-03 12:09:40 +0530

    Show password added into login page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const googlePassport = require('./passports/google')
 const passport = require('passport')
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const adminRouter = require('./routes/admin');
 
 const app = express();
 
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
