e5a0608d68c29805f32de59a6df4f3667d689890
Anas <anasna6005@gmail.com>
2023-09-15 21:10:45 +0530

    Edited and manged some bugs in admin and user side

--- controller/user/user-controller.js
+++ controller/user/user-controller.js
@@ -555,6 +555,9 @@ module.exports={
                     { minPrice: { $lte: total } }
                 )
                 Object.freeze(couponData)
+                if(productDetails.length == 0){
+                    return res.redirect('/cart');
+                }
                 res.render('user/buy-now' ,{userData , ProductDetails ,total, isSingle:true , quantity:req.session.quantity , size:req.query.size , couponData})
                 //res.render('user/buy-now' , {isSingle:true , productDetails} )
             }else{
@@ -590,6 +593,9 @@ module.exports={
                     { minPrice: { $lte: total } }
                 )
                 Object.freeze(couponData)
+                if(productDetails.length == 0){
+                    return res.redirect('/cart')
+                }
                 res.render('user/buy-now' , {productDetails , total , userData , isSingle:false , couponData})   
             }
         }catch(err){
